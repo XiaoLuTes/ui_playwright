3a8cca383ac0b9bae6a1edfd8995c527601987be
XiaoLuTes <128356770+XiaoLuTes@users.noreply.github.com>
2025-09-17 17:00:40 +0800

    新增登录重试机制

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,8 @@
+import time
+
+from selenium.common import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from pages.base_page import BasePage
 from utils.logger import logger
 from config.settings import settings
@@ -21,14 +26,40 @@ class LoginPage(BasePage):
         """初始化登录页面"""
         super().__init__(self, driver)
         # 获取配置
+        self.logger = logger
         self.config = settings
         # load_locator = ElementLocator()
         # self.locator = load_locator.load_locators()["login_page"]
 
     @allure.step("导航到登录页面")
-    def navigate_to_login(self):
+    def navigate_to_login(self, max_retry=3):
         """打开登录页面"""
         login_url = self.config.URL
-        logger.info(f"开始导航到登录页面: {login_url}")
-        self.open(login_url)
-        logger.info(f"已导航到登录页面: {login_url}")
+        locator = self.get_element_locator("username_input")
+        # logger.info(f"开始导航到登录页面: {login_url}")
+        # self.open(login_url)
+        # logger.info(f"已导航到登录页面: {login_url}")
+        for times in range(max_retry):
+            try:
+                self.logger.info(f"尝试导航到登录页面 (尝试 {times + 1}/{max_retry}次登录): {login_url}")
+                self.open(login_url)
+                # 等待页面关键元素加载完成
+                WebDriverWait(self.driver, 30).until(
+                    ec.presence_of_element_located(locator)
+                )
+                self.logger.info("成功导航到登录页面")
+                return True
+            except TimeoutException as e:
+                self.logger.warning(f"导航到登录页面超时 (尝试 {times + 1}/{max_retry}次登录): {str(e)}")
+                if times == max_retry - 1:
+                    self.logger.error("所有重试尝试均失败")
+                    raise e
+
+                time.sleep(5)
+
+            except Exception as e:
+                self.logger.error(f"打开页面时发生错误: {str(e)}")
+                if times == max_retry - 1:
+                    raise e
+            # self.driver.get(login_url)
+        return False
