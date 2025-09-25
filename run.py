2e94f33868c503b88c29736fe266f5afc442228a
XiaoLuTes <128356770+XiaoLuTes@users.noreply.github.com>
2025-09-25 19:16:00 +0800

    allure报告修复

--- run.py
+++ run.py
@@ -1,24 +1,25 @@
 import os
-import sys
+# import sys
 import pytest
+# from utils.logger import logger
 
 
-def main():
-    try:
-        # 运行测试，返回退出码
-        exit_code = pytest.main([
-            'test_from_yaml.py',
-            '-v',
-            '--junitxml=test-results.xml'
-        ])
-        # pytest.main() 返回退出码：0-成功，1-失败，其他-错误
-        sys.exit(exit_code)
-
-    except Exception as e:
-        print(f"执行测试时发生错误: {e}")
-        sys.exit(2)  # 使用2表示执行错误
+# def main():
+#     try:
+#         # 运行测试，返回退出码
+#         exit_code = pytest.main([
+#             'test_from_yaml.py',
+#             '-v',
+#             '--junitxml=test-results.xml'
+#         ])
+#         # pytest.main() 返回退出码：0-成功，1-失败，其他-错误
+#         sys.exit(exit_code)
+#
+#     except Exception as e:
+#         print(f"执行测试时发生错误: {e}")
+#         sys.exit(2)  # 使用2表示执行错误
 
 
 if __name__ == '__main__':
-    main()
-    os.system("allure generate ./temp -o ./reports/allure-reports --clean")
+    pytest.main()
+    os.system("allure generate ./reports/temp -o ./reports/allure-reports --clean")
